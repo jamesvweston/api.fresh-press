566ef3b11d475726b72b0b355f7af29defa16df4
James Weston <james@savannah.io>
2017-08-13 23:39:39 -0400

    Creating network and populating in constructor

--- app/Services/Networks/Connections/BaseNetworkConnectionService.php
+++ app/Services/Networks/Connections/BaseNetworkConnectionService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Networks\Connections;
 
 
+use App\Models\Networks\Network;
 use App\Repositories\Networks\NetworkRepository;
 use EntityManager;
 
@@ -14,9 +15,16 @@ abstract class BaseNetworkConnectionService
      */
     protected $network_repo;
 
+    /**
+     * @var Network
+     */
+    protected $network;
+
+
     public function __construct()
     {
         $this->network_repo                 = EntityManager::getRepository('App\Models\Networks\Network');
+        $this->network                      = $this->network_repo->find($this->getNetworkId());
     }
 
 
